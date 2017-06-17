2db8227c73a6a01ee49b70a89cbc3cd672fabb6e
manoj kumar <kambojmail@gmail.com>
2017-06-17 21:49:36 +0530

    Stage 1 - All HTML uploaded

---
+++ js/main.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function() {
+
+   $('#fruitscarousel').carousel({
+      interval: 10000
+    });
+        
+ $('.carousel[data-type="multi"] .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  for (var i=0;i<2;i++) {
+    next=next.next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
+  
+        
+});
+
